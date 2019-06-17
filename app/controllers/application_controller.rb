ac87cd1ef9245aad69fecde6409631f72068da8d
Matthew Serre <matthew.serre@gmail.com>
2019-06-17 23:04:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
     #your code here
     @user = User.new(:username => params[:username], :password => params[:password])
     
-    if @user.save && @user.username != nil
+    if @user.save && !@user.username.empty?
       redirect "/login"
     else
       redirect "/failure"
